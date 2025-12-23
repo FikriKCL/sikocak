af7761d16d6a82083aea4f0d80e787f8d269b1e7
FikriKCL <falfathir85@gmail.com>
2025-12-23 07:10:22 +0700

    security headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function ($middleware) {
-        // $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
+        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
     })
     ->withExceptions(function ($exceptions) {
         //
